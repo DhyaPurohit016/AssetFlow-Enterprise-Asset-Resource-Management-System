befa6ebd40f4693ff13eccfe84b440f0a3085890
Prisha Shah <prishashah2019@gmail.com>
2026-07-12 12:01:23 +0530

    Delete cloudConfig.js

--- cloudConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  cloudinary: {
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME || "",
-    apiKey: process.env.CLOUDINARY_API_KEY || "",
-    apiSecret: process.env.CLOUDINARY_API_SECRET || "",
-  },
-};
